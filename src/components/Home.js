092c4b28d6fa8868e75f8d51bda365a9a09f837d
Alberto Bort <albertobort@gmail.com>
2024-07-25 13:33:35 +0200

    Añadir enlace al CV en la sección de Inicio

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Home.css';
 import profileImage from '../assets/images/alberto_bort_pérez.JPG';
 import linkedinIcon from '../assets/images/linkedin.png';
-import githubIcon from '../assets/images/github_w.png'; // Importamos el nuevo logo
+import githubIcon from '../assets/images/github_w.png';
 import spainFlag from '../assets/images/spain.png';
 import usaFlag from '../assets/images/usa.png';
 import franceFlag from '../assets/images/france.png';
@@ -17,13 +17,14 @@ const Home = () => {
           <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
         </a>
         <a href="https://github.com/albope" target="_blank" rel="noopener noreferrer">
-          <img src={githubIcon} alt="GitHub" className="github-icon" /> {/* Añadimos el logo de GitHub */}
+          <img src={githubIcon} alt="GitHub" className="github-icon" />
         </a>
       </div>
       <p>Technical Business Analyst</p>
       <p className="description">
         QA Engineer & IT Business Analyst with more than 6 years of experience in the IT sector across various industries such as banking and public transport & mobility, combining technical knowledge with management skills to thrive in various roles within organizations dedicated to software development.
       </p>
+      <a href="https://drive.google.com/file/d/1wQlXY0ertZdb6ocfo8KQhkrDAp2lr198/view?usp=sharing" className="cv-link" target="_blank" rel="noopener noreferrer">Download My CV</a>
       <div className="languages">
         <span role="img" aria-label="Languages">🈯</span>
         <span>Languages:</span>
